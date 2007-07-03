f8ea026061f32875170fcefa131bd5cec6529aef
jeremywales <jeremywales@f19de1e5-8731-0410-8c64-333063c434b8>
2007-07-03 12:55:23 +0000

    Added DesignPoliciesTest with a universal check for null parameter rejection. Added a CheckStyle check to enforce final fields.

--- transloader/src/main/java/com/googlecode/transloader/AcceptsNullMethodParams.java
+++ transloader/src/main/java/com/googlecode/transloader/AcceptsNullMethodParams.java
@@ -2,7 +2,9 @@ package com.googlecode.transloader;
 
 /**
  * A marker interface which indicates that the implementing <code>Class</code> accepts <code>null</code> for method
- * parameters.
+ * parameters. The convention within Transloader for all <code>Class</code>es which do <b>not</b> implement this
+ * <code>interface</code> is to use {@link Assert} which will throw an {@link IllegalArgumentException} for any
+ * parameter which is <code>null</code>.
  * 
  * @author Jeremy Wales
  */
